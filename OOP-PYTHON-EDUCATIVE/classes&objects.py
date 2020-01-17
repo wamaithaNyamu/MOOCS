82450dbee983550261b6d737aba7a9417b61fa57
Wamaitha Nyamu <wamaithanyamu@gmail.com>
2020-01-17 11:10:10 +0300

    Class and Objects
    Finished the section.

--- OOP-PYTHON-EDUCATIVE/classes&objects.py
+++ OOP-PYTHON-EDUCATIVE/classes&objects.py
@@ -74,3 +74,135 @@ class Player:
 
 p1 = Player('Mark')
 p2 = Player('Ken')
+# METHODS IN A CLASS-------------------------------------------------
+class Employee:
+    # defining the initializer
+    def __init__(self, ID=None, salary=None, department=None):
+        self.ID = ID
+        self.salary = salary
+        self.department = department
+
+    def tax(self):
+        return (self.salary * 0.2)
+
+    def salaryPerDay(self):
+        return (self.salary / 30)
+
+
+# initializing an object of the Employee class
+Steve = Employee(3789, 2500, "Human Resources")
+
+# METHOD OVERLOADING...............................
+class Employee:
+    def __init__(self, ID=None, Salary=None, dpt=None):
+        self.ID =ID
+        self.Salary = Salary
+        self.dpt= dpt
+
+        # method overload
+    def demo(self, a,b,c,d=5, e=None):
+        print("a is" , a)
+        print("b is" , b)
+        print("c is" , c)
+        print("d is" , d)
+        print("e is" , e)
+
+    def tax(self, title = None):
+        return(self.Salary * 0.2)
+
+    def salaryPerDay(self):
+        return (self.Salary / 30)
+
+Steve = Employee(123,1234567,'Finance')
+
+
+# Printing properties of Steve
+# print ("Demo 1")
+# Steve.demo(1, 2, 3);
+# print("\n")
+#
+# print ("Demo 2")
+# Steve.demo(1, 2, 3, 4);
+# print("\n")
+#
+# print ("Demo 3")
+# Steve.demo(1, 2, 3, 4, 5);
+
+# CLASS METHODS AND STATIC METHODS--------------------------------------------
+# there are three types of methods, instance methods class ethods and static methods
+# CLASS METHODS
+class MyClass:
+    classVariable = 'Educative'
+
+    @classmethod
+    def demo(cls):
+        return cls.classVariable
+
+
+
+class Player:
+    team = 'Liverpool'
+    def __init__(self, name):
+        self.name = name
+
+    @classmethod
+    def getTeamName(cls):
+       return  cls.team
+
+Alex = Player('alex')
+
+# print(Alex.name)
+# print(Alex.getTeamName())
+#
+
+
+# STATIC METHODS
+class Player:
+    team = 'Liverpool'
+
+    def __init__(self, name):
+        self.name = name
+
+    @staticmethod
+
+    def demo():
+        print('Static method')
+
+
+x =Player('Alex')
+x.demo()
+Player.demo()
+#ACCESS MODIFIERS
+# access restrictions are imposed using access modifiers
+# they are tags that determine which part of the program can be accessed directly
+# PRIVATE ATTRIBUTES
+class MyClass:
+    def __init__(self, salary):
+        self.__salary = salary #private attribute that cant be accessed outside the class
+
+# print(MyClass.__salary)  this throws an error
+# PRIVATE METHODS
+class Employee:
+    def __init__(self, ID, salary):
+        self.ID = ID
+        self.__salary = salary  # salary is a private property
+
+    def displaySalary(self):  # displaySalary is a public method
+        print("Salary:", self.__salary)
+
+    def __displayID(self):  # displayID is a private method
+        print("ID:", self.ID)
+
+
+Steve = Employee(3789, 2500)
+Steve.displaySalary()
+# Steve.__displayID()  # this will generate an error
+
+# when its extremely necessary to access private property or method we use the _<classname> prefix
+class Employee:
+  def __init__(self, ID, salary):
+    self.ID = ID
+    self.__salary = salary # salary is a private property
+
+Steve = Employee(3789, 2500)
+print(Steve._Employee__salary) #accessing a private property
